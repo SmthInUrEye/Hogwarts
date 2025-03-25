7854e0ab1762fe3f96ca9f56b2017926e530d1db
SmthInUrEye <dragon-pg@yandex.ru>
2025-03-25 17:08:01 +0300

    HW-1 (добавить фильтрацию)

---
+++ src/main/java/ru/hogwarts/school/controller/FacultyController.java
@@ -0,0 +1,53 @@
+package ru.hogwarts.school.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import ru.hogwarts.school.model.Faculty;
+import ru.hogwarts.school.service.FacultyService;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping("faculty")
+public class FacultyController {
+  private final FacultyService facultyService;
+
+  public FacultyController(FacultyService facultyService) {
+    this.facultyService = facultyService;
+  }
+
+  @GetMapping("{id}")
+  public ResponseEntity<Faculty> getFacultyInfo(@PathVariable Long id) {
+    Faculty foundFaculty = facultyService.findFaculty(id);
+    if (foundFaculty == null) {
+      return ResponseEntity.notFound().build();
+    }
+    return ResponseEntity.ok(foundFaculty);
+  }
+
+  @PostMapping
+  public Faculty createFaculty(@RequestBody Faculty faculty) {
+    return facultyService.createFaculty(faculty);
+  }
+
+  @PutMapping
+  public ResponseEntity<Faculty> editFaculty(@RequestBody Faculty faculty) {
+    Faculty foundFaculty = facultyService.editFaculty(faculty);
+    if (foundFaculty == null) {
+      return ResponseEntity.notFound().build();
+    }
+    return ResponseEntity.ok(foundFaculty);
+  }
+
+  @DeleteMapping("{id}")
+  public Faculty deleteFaculty(@PathVariable Long id) {
+    return facultyService.deleteFaculty(id);
+  }
+
+  @GetMapping
+  public ResponseEntity <Collection<Faculty>> getAllFaculties () {
+    return ResponseEntity.ok(facultyService.getAllFaculties());
+  }
+
+}
